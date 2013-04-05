81252e01d0f3d77ba1fe57a5fbd46750872909c5
Daniel Gardner <dangard@runbox.com>
2013-04-04 20:32:53 -0700

    Controller updates.

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -3,6 +3,16 @@
 class Home extends CI_Controller {
 	public function index ()
 	{
-		$this->load->view('home');
+		$this->load->model('jobs_model');
+		$data['rows'] = $this->jobs_model->getAll();
+		$data['date'] = date('Y');
+		$data['title'] = 'Blah blah blah!!!';
+        $data['main_content'] = 'home.tpl';
+		$this->smarty->view( 'includes/template.tpl', $data );
 	}
+
+    public function demo() {
+        $data['main_content'] = 'demo.tpl';
+        $this->smarty->view( 'includes/template.tpl', $data );
+    }
 }
