6487693ac9925ab738452e0827652f6a7478e3bf
Daniel Gardner <dangard@runbox.com>
2021-09-17 17:42:10 -0700

    New Angular version of the site.

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'CamelBird';
+  date = new Date();
+  year = this.date.getFullYear();
+}
