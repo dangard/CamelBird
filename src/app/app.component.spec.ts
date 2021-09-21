84d4080383042237ad35ddf25e62ae6be528f656
Daniel Gardner <dangard@runbox.com>
2021-09-20 20:15:23 -0700

    Added eslint and prettier task. Ran eslint and prettier task and committing  linted code.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,48 +3,48 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [RouterTestingModule],
+            declarations: [AppComponent],
+        }).compileComponents();
+    });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+    it('should create the app', () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.componentInstance;
+        expect(app).toBeTruthy();
+    });
 
-  it(`should have as title 'CamelBird'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('CamelBird');
-  });
-    
-  it(`should have year a '2021'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    const currentYear = new Date().getFullYear();  
-    expect(app.year).toEqual(currentYear);
-  });
+    it(`should have as title 'CamelBird'`, () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.componentInstance;
+        expect(app.title).toEqual('CamelBird');
+    });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.toolbar span')?.textContent).toContain('CamelBird.com');
-  });
-           
-  it('should render year', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    const currentYear = new Date().getFullYear();
-    expect(compiled.querySelector('footer')?.textContent).toContain(currentYear);
-  });
+    it(`should have year a '2021'`, () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.componentInstance;
+        const currentYear = new Date().getFullYear();
+        expect(app.year).toEqual(currentYear);
+    });
+
+    it('should render title', () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        fixture.detectChanges();
+        const compiled = fixture.nativeElement as HTMLElement;
+        expect(compiled.querySelector('.toolbar span')?.textContent).toContain(
+            'CamelBird.com'
+        );
+    });
+
+    it('should render year', () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        fixture.detectChanges();
+        const compiled = fixture.nativeElement as HTMLElement;
+        const currentYear = new Date().getFullYear();
+        expect(compiled.querySelector('footer')?.textContent).toContain(
+            currentYear
+        );
+    });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'CamelBird';
-  date = new Date();
-  year = this.date.getFullYear();
+    title = 'CamelBird';
+    date = new Date();
+    year = this.date.getFullYear();
 }
