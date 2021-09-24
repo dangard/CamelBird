bf93a39d53b14c4bf6cf0ee9eea7cf8342ad6a40
Daniel Gardner <dangard@runbox.com>
2021-09-23 20:34:55 -0700

    Fixing up and refactoring the unit tests.

--- src/app/shared/components/footer/footer.component.spec.ts
+++ src/app/shared/components/footer/footer.component.spec.ts
@@ -4,13 +4,13 @@ import { FooterComponent } from './footer.component';
 
 describe('FooterComponent', () => {
   let component: FooterComponent;
-  let fixture: ComponentFixture<FooterComponent>;
+  let fixture: ComponentFixture < FooterComponent > ;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FooterComponent ]
-    })
-    .compileComponents();
+        declarations: [FooterComponent]
+      })
+      .compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +21,20 @@ describe('FooterComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it(`should have year a '2021'`, () => {
+    const fixture = TestBed.createComponent(FooterComponent);
+    const app = fixture.componentInstance;
+    const currentYear = new Date().getFullYear();
+    expect(app.year).toEqual(currentYear);
+  });
+
+  it('should render year', () => {
+    const fixture = TestBed.createComponent(FooterComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    const currentYear = new Date().getFullYear();
+    expect(compiled.querySelector('footer')?.textContent).toContain(currentYear);
   });
 });
