8651f0d7003c9bda2b30e9e2ff4f5e458bd26081
Daniel Gardner <dangard@runbox.com>
2021-09-23 18:29:01 -0700

    Refactor homepage html and CSS to use the new header and foots components.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-    title = 'CamelBird';
     date = new Date();
     year = this.date.getFullYear();
 }
