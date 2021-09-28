9e71a98648df2ad64bbe6d230856c074123620b7
Daniel Gardner <dangard@runbox.com>
2021-09-27 17:06:18 -0700

    Ensuring the create devblog component is imported and small menu css tweak.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 import { DevblogComponent } from './pages/devblog/devblog.component';
 import { AccomplishmentsComponent } from './pages/accomplishments/accomplishments.component';
 import { InterestsComponent } from './pages/interests/interests.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { DevblogListComponent } from './shared/components/devblog-list/devblog-list.component';
+import { DevblogCreatorComponent } from './shared/components/devblog-creator/devblog-creator.component';
 
 @NgModule({
-    declarations: [AppComponent, DevblogComponent, AccomplishmentsComponent, InterestsComponent, HeaderComponent, FooterComponent, DevblogListComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    declarations: [AppComponent, DevblogComponent, AccomplishmentsComponent, InterestsComponent, HeaderComponent, FooterComponent, DevblogListComponent, DevblogCreatorComponent],
+    imports: [BrowserModule, AppRoutingModule, FormsModule],
     providers: [],
     bootstrap: [AppComponent],
 })
