64666fca86b91bfc60ddb5d5575c0d57662f5c07
Daniel Gardner <dangard@runbox.com>
2021-09-29 17:18:28 -0700

    Wiring in the Event Listener to trigger an event that the devblog list can listen for and rerender when a new devblog is added.

--- src/app/shared/components/devblog-list/devblog-list.component.ts
+++ src/app/shared/components/devblog-list/devblog-list.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy  } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { DevblogService } from '../../services/devblog/devblog.service';
+import { EventListenerService } from '../../services/common/event-listener.service';
+import { AppConstants } from '../../../core/app.constants';
+
 import * as moment from 'moment';
 
 @Component({
@@ -7,16 +11,31 @@ import * as moment from 'moment';
   templateUrl: './devblog-list.component.html',
   styleUrls: ['./devblog-list.component.scss']
 })
-export class DevblogListComponent implements OnInit {
+export class DevblogListComponent implements OnInit, OnDestroy {
+  private eventListenerSubject: any;
   devBlogs: any;
   selectedDevBlog: any;
+  event: any;
 
-  constructor(public dataService: DevblogService) { }
+  constructor(private constants: AppConstants, public dataService: DevblogService, private eventListenerService: EventListenerService) {
+    // subscribe to sender component messages
+    this.eventListenerSubject = this.eventListenerService.getUpdate().subscribe
+      (message => { //message contains the data sent from service
+      this.event = message;
+      if (this.event.text === this.constants.EVENTS.DEVBLOG.CREATE) {
+        this.getGetDevBlogs();
+      }
+    });
+  }
 
   ngOnInit() {
+    this.getGetDevBlogs();
+  }
+
+  private getGetDevBlogs() {
     this.dataService.getDevBlogs().subscribe(resp =>{
       this.devBlogs = resp;
-    })  
+    });
   }
 
   public formatDate(date: string) {
@@ -25,5 +44,9 @@ export class DevblogListComponent implements OnInit {
 
   public selectDevBlog(devBlog: any) {
     this.selectedDevBlog = devBlog;
+  }
+
+  ngOnDestroy() {
+    this.eventListenerSubject.unsubscribe();
   }
 }
--- src/app/shared/services/devblog/devblog.service.ts
+++ src/app/shared/services/devblog/devblog.service.ts
@@ -7,6 +7,7 @@ import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
 import { AppConstants } from '../../../core/app.constants';
 import { environment } from '../../../../environments/environment';
+import { EventListenerService } from '../common/event-listener.service';
 
 @Injectable({
   providedIn: 'root'
@@ -21,22 +22,28 @@ export class DevblogService {
   now = moment("").format("YYYY-MM-DD HH:MM:SS");
   errorMessage: string = "Ooops";
   postId: any;
+  eventText: string = '';
 
 
-  constructor(private http: HttpClient, private constants: AppConstants) {
+  constructor(private http: HttpClient, private constants: AppConstants, private eventListenerService: EventListenerService) {
     this.apiServerUrl = environment.apiServerUrl;
-    this.getDevLogsUrl = this.constants.OPERATIONS.DEVLOG.GET_ALL;
+    this.getDevLogsUrl = this.constants.OPERATIONS.DEVBLOG.GET_ALL;
   }
 
   public getDevBlogs() {
-    return this.http.get(this.REST_API_SERVER + this.constants.OPERATIONS.DEVLOG.GET_ALL);
+    let res = this.http.get(this.REST_API_SERVER + this.constants.OPERATIONS.DEVBLOG.GET_ALL);
+    this.eventText = this.constants.EVENTS.DEVBLOG.READ;
+    this.sendMessage();
+    return res;
   }
   
   public createDevBlog(devBlog: { title:string, body:string, user:string }) {
     console.log(devBlog);
-    this.http.post<any>(this.REST_API_SERVER + this.constants.OPERATIONS.DEVLOG.CREATE, devBlog).subscribe({
+    this.http.post<any>(this.REST_API_SERVER + this.constants.OPERATIONS.DEVBLOG.CREATE, devBlog).subscribe({
         next: data => {
             this.postId = data.log_id;
+            this.eventText = this.constants.EVENTS.DEVBLOG.CREATE;
+            this.sendMessage();
             //TODO RefreshView
         },
         error: error => {
@@ -44,5 +51,9 @@ export class DevblogService {
             console.error('There was an error!', error);
         }
     })
+  }
+
+  sendMessage(): void {
+    this.eventListenerService.sendUpdate(this.eventText);
   }
 }
