37596a6f5411c79915e37f7ae63615e194e735b9
Daniel Gardner <dangard@runbox.com>
2021-10-09 13:48:08 -0700

    Updating the header to use bootstrap-icons and adding ngx-bootstrap-icons to the app.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgxBootstrapIconsModule } from 'ngx-bootstrap-icons';
+import { github, linkedin } from 'ngx-bootstrap-icons';
 import { DevblogComponent } from './pages/devblog/devblog.component';
 import { AccomplishmentsComponent } from './pages/accomplishments/accomplishments.component';
 import { InterestsComponent } from './pages/interests/interests.component';
@@ -14,9 +16,15 @@ import { DevblogCreatorComponent } from './shared/components/devblog-creator/dev
 import { FaceOffCritiqueComponent } from './shared/components/face-off-critique/face-off-critique.component';
 import { CamelbirdComponent } from './pages/camelbird/camelbird.component';
 
+
+const icons = {
+  github,
+  linkedin
+};
+
 @NgModule({
     declarations: [AppComponent, DevblogComponent, AccomplishmentsComponent, InterestsComponent, HeaderComponent, FooterComponent, DevblogListComponent, DevblogCreatorComponent, FaceOffCritiqueComponent, CamelbirdComponent],
-    imports: [BrowserModule, HttpClientModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
+    imports: [BrowserModule, HttpClientModule, AppRoutingModule, FormsModule, ReactiveFormsModule, NgxBootstrapIconsModule.pick(icons)],
     providers: [],
     bootstrap: [AppComponent],
 })
